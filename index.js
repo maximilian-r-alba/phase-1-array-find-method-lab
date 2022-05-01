b067387718dd4ab8e165d9dbc6479516e486466f
Maximilian Alba <albam476@gmail.com>
2022-05-01 19:00:17 -0400

    commit tree d9ac16356fa96e3906dbb1dca857fc552fdd9615

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array){
+    const winYear = array.find(function (element){
+        return element.result === 'W';
+    })
+    if (winYear === undefined){
+        return undefined
+    }
+    else{
+        return winYear.year
+    }
+}
